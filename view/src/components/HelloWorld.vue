5312b6c200414108451d98f20c5d01f13f166027
Samuel Silva <samuel.ms2112@gmail.com>
2021-03-19 22:52:15 -0300

    commit 19/03/2021 22:51

--- view/src/components/HelloWorld.vue
+++ view/src/components/HelloWorld.vue
@@ -1,59 +1,75 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Tarefa</th>
-          <th>Desk</th>
-          <th>Respo</th>
-        </tr>
-      </thead>
-      <tbody id="listaProdutos">
-         <tr v-for="item in lista" is="pd" :key="item.tarefa" :tarefa="item.tarefa" :descricao="item.descricao" :responsavel="item.responsavel"></tr>
-      </tbody>
-    </table>
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>Ola Mundo</h1>
+      <ol class="breadcrumb">
+        <li>
+          <a href="/"><i class="fa fa-home"></i> Home</a>
+        </li>
+        <li class="active">Ola Mundo</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Lista</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body no-padding">
+          <table class="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th style="width: 10px">#</th>
+                <th>NOME</th>
+                <th>EMAIL</th>
+                <th>DOCUMENTO</th>
+                <th style="min-width: 75px">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="fornecedor of fornecedores" :key="fornecedor.id">
+                <td>1.</td>
+                <td>{{ fornecedor.Nome }}</td>
+                <td>{{ fornecedor.email }}</td>
+                <td>{{ fornecedor.documento }}</td>
+                <td>
+                  <button
+                    type="button"
+                    class="btn btn-xs btn-danger btn-delete"
+                  >
+                    <i class="fa fa-trash"></i> Excluir
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <!-- /.box-body -->
+      </div>
+    </section>
+    <!-- /.content -->
   </div>
 </template>
 
 <script>
-import Task from '../services/task'
-import Vue from 'vue'
+import Fornecedor from "../services/fornecedor";
 
-Vue.component('pd', {
-  props: ['id', 'tarefa', 'descricao', 'responsavel'],
-  template: '<tr><td>{{ id }}</td><td>{{ tarefa }}</td><td>{{ descricao }}</td><td>{{ responsavel }}</td></tr>'
-});
-
-var app = new Vue({
-  el: '#listaProdutos',
-  data: {
-    lista: []
-  }
-
-});
 export default {
-  
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      fornecedores: [],
+    };
   },
-
-  // date(){
-  //   return {
-  //     lista: []
-  //   }
-  // },
-
-  mounted(){
-    Task.listar().then(res => {
-      console.log(res.data)
-      app.lista = res.data
-      console.log("Lista -> :" + app.lista);
+  mounted() {
+    Fornecedor.listar().then((res) => {
+      this.fornecedores = res.data;
+      console.log("samuel")
     });
   },
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- view/src/main.js
+++ view/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Teste from './components/teste.vue'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [{
+            path: '/teste',
+            component: Teste
+        },
+        {
+            path: '/',
+            component: HelloWorld
+        }
+    ]
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
