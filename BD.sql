5bfc5f8111ba0804eebdc25fe4417c3db66b4f14
andrebossi <andreluizbossi70@gmail.com>
2021-04-09 20:18:15 -0300

    modify db ddl

--- BD.sql
+++ BD.sql
@@ -1,25 +1,25 @@
-CREATE DATABASE projeto;
+CREATE DATABASE IF NOT EXISTS projeto;
 USE projeto;
 
-create table pessoa(
-	id int primary key auto_increment not null,
-	documento varchar(20) unique not null,
-    nome varchar(225) not null,
-    nomeFantasia varchar(225)
-    data_nasc varchar(12) not null,
-    email varchar(225) not null,
-    tipoPessoa boolean not null, /*--> Fisica ou Juritica <--*/
-    tipoCadastro boolean not null /*--> Cliente ou Fornecedor <--*/
+CREATE TABLE pessoa(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	documento VARCHAR(20) UNIQUE NOT NULL,
+   nome VARCHAR(225) NOT NULL,
+   nomeFantasia VARCHAR(225),
+   data_nasc VARCHAR(12) NOT NULL,
+   email VARCHAR(225) NOT NULL,
+   tipoPessoa boolean NOT NULL, /*--> Fisica ou Juritica <--*/
+   tipoCadastro boolean not null /*--> Cliente ou Fornecedor <--*/
 );
 
-create table contato(
+CREATE TABLE contato(
 	id int primary key auto_increment not null,
-    telefone varchar(30) not null,
-    idPessoa int not null,
-    constraint foreign key FK_ID_P_C (idPessoa) references pessoa(id)
+   telefone varchar(30) not null,
+   idPessoa int not null,
+   constraint foreign key FK_ID_P_C (idPessoa) references pessoa(id)
 );
 
-create table endereco(
+CREATE TABLE endereco(
 	id int primary key auto_increment not null,
     rua varchar(30) not null,
     numero varchar(30) not null,
@@ -31,3 +31,52 @@ create table endereco(
     constraint foreign key FK_ID_P_E (idPessoa) references pessoa(id)
 );
 
+CREATE TABLE grupo (
+    `id` BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE subgrupo (
+    `id` BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(40) NOT NULL,
+    `isfracioned` BIT COMMENT '0 inteiro \n 1 fracionado'
+);
+
+CREATE TABLE pedido (
+    `id` BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `fornecedor_id` BIGINT(20) UNSIGNED NOT NULL,
+    `datacompra` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `qtde` DECIMAL(10,2),
+    `valorpago` DECIMAL(15,2) NOT NULL
+);
+
+CREATE TABLE labengprod (
+    `id` BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `ean`  VARCHAR(13),
+    `grupo_id` BIGINT(20) UNSIGNED,
+    `subgrupo_id` BIGINT(20) UNSIGNED,
+    `preco` DECIMAL(15,2) NOT NULL,
+    `estoque_id` BIGINT(20) UNSIGNED NOT NULL,
+
+    FOREIGN KEY (grupo_id) REFERENCES grupo(id),
+    FOREIGN KEY (subgrupo_id) REFERENCES subgrupo(id),
+    FULLTEXT (`name`),
+    FULLTEXT (`ean`)
+);
+
+CREATE TABLE estoque (
+    `id` BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `prod_id` BIGINT(20) UNSIGNED NOT NULL,
+    -- Definir regra se produto e fracionado ou inteiro
+    `qtde` DECIMAL(10,2), FOREIGN KEY (prod_id) REFERENCES labengprod(id)
+);
+
+-- Relacionamento N pra N fornecedor produto
+CREATE TABLE fornecedorprod (
+    `id` BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `prod_id` BIGINT(20) UNSIGNED NOT NULL,
+    `fornecedor_id` BIGINT(20) UNSIGNED NOT NULL,
+    `creation_time` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `modification_time` DATETIME ON UPDATE CURRENT_TIMESTAMP
+);
