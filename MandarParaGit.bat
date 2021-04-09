4a77f567af215f510eab01e02e89a064306cd8ed
Samuel Silva <samuel.ms2112@gmail.com>
2021-04-08 23:24:54 -0300

    Commit

--- MandarParaGit.bat
+++ MandarParaGit.bat
@@ -1,8 +1,4 @@
-
-@echo off
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 git add .
-git commit -m "Commit %mydate% %mytime%"
+git commit -m "Commit"
 git push
 pause
