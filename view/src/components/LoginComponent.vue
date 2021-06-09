5a94609e34e39f442a8a107ca9f0abff368fa1c5
Samuel Silva <samuel.ms2112@gmail.com>
2021-06-08 23:17:11 -0300

    Commit

--- view/src/components/LoginComponent.vue
+++ view/src/components/LoginComponent.vue
@@ -44,10 +44,10 @@
                 Login.authenticate(this.Login).then((response) => {
                     if(response.status == 200) {
                       store.state.user.authenticated = true
-                      localStorage.setItem('token', this.Login.email)
+                      this.$router.push({ name: 'Fornecedor'})
+                      //localStorage.setItem('token', this.Login.email)
                     }
-
-                    this.$router.push({ name: 'Fornecedor'})
+                    //this.$router.push({ name: 'Fornecedor'})
                 }).catch(() => {
                   store.state.user.authenticated = false
                     alert("Dados incorretos");
